cf629822d5b03bc880ce4bfd34da3a4f30c7aa50
Alex Laursen <ajlaursen@aol.com>
2021-06-04 19:02:42 -0400

    ListItem component

--- App.js
+++ App.js
@@ -7,19 +7,10 @@ import { MaterialCommunityIcons, Zocial } from "@expo/vector-icons";
 import AppText from "./app/components/AppText/AppText";
 import LoginButton from "./app/components/LoginButton/LoginButton";
 import Card from "./app/components/Card/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 let chairImage = require("./app/assets/chair.jpg");
 
 export default function App() {
-	return (
-		<View
-			style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}
-		>
-			<Card
-				title="Red Jacket for sale"
-				subTitle="$100"
-				image={chairImage}
-			/>
-		</View>
-	);
+	return <ListingDetailsScreen></ListingDetailsScreen>;
 }
