47ed989755fb121dd1b98b35238432090e3b2476
Alex Laursen <ajlaursen@aol.com>
2021-06-03 22:10:37 -0400

    update

--- App.js
+++ App.js
@@ -1,68 +1,39 @@
 import React from "react";
-import {
-	StyleSheet,
-	Text,
-	View,
-	SafeAreaView,
-	Image,
-	TouchableWithoutFeedback,
-	TouchableOpacity,
-	TouchableHighlight,
-	Button,
-	Alert,
-	Platform,
-	StatusBar,
-	Dimensions,
-} from "react-native";
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-	let x = 1;
-
-	const handlePress = () => console.log("text clicked");
-	const {landscape} = useDeviceOrientation()
-
 	return (
-		<SafeAreaView style={styles.container}>
+		<View
+			style={{
+				backgroundColor: "#fff",
+				flex: 1,
+				flexDirection: "row",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
 			<View
 				style={{
 					backgroundColor: "dodgerblue",
-					width: "100%",
-					height: landscape ? "100%" : "30%"
+					width: 100,
+					height: 100,
+					alignSelf: "flex-start"
 				}}
-			></View>
-			{/* <Text numberOfLines={1} onPress={handlePress}>
-				Hello World again
-			</Text>
-			<TouchableHighlight onPress={() => console.log("pressed Image")}>
-				<Image
-					blurRadius={0.75}
-					source={{
-						width: 150,
-						height: 150,
-						uri: "https://via.placeholder.com/150",
-					}}
-				/>
-			</TouchableHighlight>
-			<Button
-				title={"press me"}
-				onPress={() =>
-					Alert.prompt("my title", "my message", (text) =>
-						console.log(text)
-					)
-				}
-			></Button>
-			<StatusBar style="auto" /> */}
-		</SafeAreaView>
+			/>
+			<View
+				style={{
+					backgroundColor: "gold",
+					width: 100,
+					height: 100,
+				}}
+			/>
+			<View
+				style={{
+					backgroundColor: "tomato",
+					width: 100,
+					height: 100,
+				}}
+			/>
+		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		// alignItems: "center",
-		// justifyContent: "center",
-		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-	},
-});
---
+++ past.js
@@ -0,0 +1,29 @@
+<View
+style={{
+  backgroundColor: "dodgerblue",
+  width: "100%",
+  height: landscape ? "100%" : "30%"
+}}
+></View>
+{/* <Text numberOfLines={1} onPress={handlePress}>
+Hello World again
+</Text>
+<TouchableHighlight onPress={() => console.log("pressed Image")}>
+<Image
+  blurRadius={0.75}
+  source={{
+    width: 150,
+    height: 150,
+    uri: "https://via.placeholder.com/150",
+  }}
+/>
+</TouchableHighlight>
+<Button
+title={"press me"}
+onPress={() =>
+  Alert.prompt("my title", "my message", (text) =>
+    console.log(text)
+  )
+}
+></Button>
+<StatusBar style="auto" /> */}
