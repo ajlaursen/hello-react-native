ad6830a01b6360da0e6398f2c13205fac54fbaf3
Alex Laursen <ajlaursen@aol.com>
2021-06-04 13:02:00 -0400

    shadows and boreders

--- App.js
+++ App.js
@@ -4,5 +4,25 @@ import WelcomeScreen from "./app/screens/WelcomScreens";
 import VeiwImageScreen from "./app/screens/VeiwImageScreen";
 
 export default function App() {
-	return <VeiwImageScreen></VeiwImageScreen>;
+	return (
+		<View
+			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+		>
+			<View
+				style={{
+					backgroundColor: "dodgerblue",
+					width: 100,
+					height: 100,
+					borderWidth: 10,
+					borderColor: "royalblue",
+					borderRadius: 50,
+					shadowColor: "grey",
+					shadowOffset: { width: -10, height: 10 },
+					shadowOpacity: 0.5,
+					shadowRadius: 5,
+					elevation: 10,
+				}}
+			></View>
+		</View>
+	);
 }
